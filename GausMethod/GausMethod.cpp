1cb9435fbe8c6529facb003f0039c567d0f4be8f
Vladislakt <vlad1.999vlad@mail.ru>
2024-02-26 01:42:44 +0300

    Сделано в сыром виде без чтения и вывода в файл

--- GausMethod/GausMethod.cpp
+++ GausMethod/GausMethod.cpp
@@ -1,26 +1,25 @@
 ﻿#include <iostream>
+#include <list>
 using  namespace std;
 
-int** getMatrix() {
-    int n = 2;
-    int m = 4;
-    int** M = new int* [n];
+void printMatrix(double** M, int n, int m) {
     for (int i = 0; i < n; i++) {
-        M[i] = new int[m + 1];
+        for (int j = 0; j < m; j++) {
+            cout << M[i][j] << " ";
+        }
+        cout << endl;
     }
-    M[0][0] = 1; M[0][1] = 2; M[0][2] = 5; M[0][3] = -1; M[0][4] = 4;
-    M[1][0] = 1; M[1][1] = -1; M[1][2] = -1; M[1][3] = 2; M[1][4] = 1;
-    return M;
+    return;
 }
 
-void printSystem(int** M, int n, int m) {
+void printSystem(double** M, int n, int m) {
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m + 1; j++) {
+        for (int j = 0; j < m; j++) {
             if (M[i][j] < 0) {
                 cout << " - " << -1 * M[i][j];
             }
             else {
-                if (j != 0 && j != m) {
+                if (j != 0 && j != m - 1) {
                     cout << " + " << M[i][j];
                 }
                 else cout << M[i][j];
@@ -29,7 +28,7 @@ void printSystem(int** M, int n, int m) {
             if (j < m - 1) {
                 cout << "*x_" << j + 1;
             }
-            if (j == m - 1) {
+            if (j == m - 2) {
                 cout << " = ";
             }
         }
@@ -38,11 +37,61 @@ void printSystem(int** M, int n, int m) {
     return;
 }
 
+bool contains(list<int> Indexes, int index) {
+    for (int i : Indexes) {
+        if (index == i - 1) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main()
 {
-    int n = 2, m = 4;
-    int** M = getMatrix();
-    printSystem(M, n, m);
+    int n = 2, m = 5;
+    double** M;
+    M = new double*[n];
+    for (int i = 0; i < n; i++) {
+        M[i] = new double[m];
+    }
+    M[0][0] = 1; M[0][1] = 2; M[0][2] = 5; M[0][3] = -1; M[0][4] = 1;
+    M[1][0] = 1; M[1][1] = -1; M[1][2] = -1; M[1][3] = 2; M[1][4] = 1;
+    list<int> Indexes;
+    Indexes.push_front(1);
+    Indexes.push_front(2);
+
+    printMatrix(M, n, m);
+    cout << "-----------------------------" << endl;
+
+    double mn;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (contains(Indexes, j)) {
+                continue;
+            }
+            else {
+
+
+                if (M[i][j] != 0) {
+                    mn = M[i][j];
+                    for (int k = 0; k < m; k++) {
+                        M[i][k] = M[i][k] / mn;
+                    }
+                    for (int k = 0; k < n; k++) {
+                        if (k != i) {
+                            mn = M[k][j];
+                            for (int l = 0; l < m; l++) {
+                                M[k][l] = M[k][l] - mn * M[i][l];
+                            }
+                        }
+                    }
+                    break;
+                }
+            }            
+        }
+    }
+    printMatrix(M, n, m);
     return 0;
 }
 
